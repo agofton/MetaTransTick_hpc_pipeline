f53d1f37b0bdf60ff313c89554c7f19acd2d6c0d
Alex Gofton <alexander.gofton@gmail.com>
2020-06-25 09:35:40 +1000

    cleaning up code in init.sh, adding module functionality to bin/run.sh

--- bin/run.sh
+++ bin/run.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
+date
+
+helpMessage() {
+	echo "$0 usage: -s <all, derep, trinityP1, trinityP2, trinityP3, blast+diamond, onlyQC, onlyDerep, onlyTrinity, onlyTrinityP1, onlyTrinityP2, onlyTrinityP3, onlyBlast, onlyDiamond> -h [show this message]"
+	echo 'Running with -s flags "all", "derep",  "trinityP1", "trinityP2", "trinityP3", or "blast+diamond" will start the pipeline at that step and then run to the end.'
+	echo 'Running with -s flags begining with "only" with run only that module.'
+}
 
 flowControl() {
 	if [ $? -ne 0 ]
 	then
 		echo $1; date; exit 1
-	else
-		echo $2
 	fi
 }
 
-source slurmParams.txt
-flowControl "Cannot source variables from ./slurmParams.txt. File does not exits." " "
-
-echo ${qcJobName}
-
+source slurmParams.txt && flowControl "Cannot source variables from ./slurmParams.txt. File does not exits."
 
 # job listing
 qc=QC.SAMPLEID.slurm.sh
@@ -24,66 +25,271 @@ trinP3=trinity_P3.SAMPLEID.slurm.sh
 blastnTrin=blast_trin.SAMPLEID.slurm.sh
 diamTrin=diam_trin.SAMPLEID.slurm.sh
 
-# QC - no dependencies, start asap.
-job1=$(sbatch -J ${qcJobName} -N ${qcNodes} -n ${qcTasks} -c ${qcCPUsPerTask} -t ${qcTime} --mem ${qcMem} -o ${qcLog} ${qc})
+# Command line arguments
+while getopts "hs:" OPT
+do
+	case "${OPT}"
+		in
+	        h) helpMessage;;
+	        s) steps=${OPTARG};;
+	esac
+done
+shift $((OPTIND - 1))
 
-	job1ID=$(sed 's/Submitted batch job //g' <<< ${job1})
-	
-	echo ""
-	echo "Job 1: Quality Trimming; ${qc} queued with jobid=${job1ID}."
-	echo ""
+if [ "${step}" == "all" ]
+then
+	# QC - no dependency
+	job1=$(sbatch -J ${qcJobName} -N ${qcNodes} -n ${qcTasks} -c ${qcCPUsPerTask} -t ${qcTime} --mem ${qcMem} -o ${qcLog} ${qc})
+		job1ID=$(sed 's/Submitted batch job //g' <<< ${job1})
+			echo ""
+			echo "Job 1: Quality Trimming; ${qc} queued with jobid=${job1ID}."
+			echo ""
+	# derep  - dependent on QC
+	job2=$(sbatch --dependency=afterok:${job1ID} -J ${drJobName} -N ${drNodes} -n ${drTasks} -c ${drCPUsPerTask} -t ${drTime} --mem ${drMem} -o ${drLog} ${derep})
+		job2ID=$(sed 's/Submitted batch job //g' <<< ${job2})
+			echo "Job 2: Dereplication; ${derep} queued with jobid=${job2ID}."
+			echo "${derep} will begin after successfull completion of ${qc}."
+			echo ""
+	# trinity phase 1 - dependent on derep
+	job3=$(sbatch --dependency=afterok:${job2ID} -J ${t1JobName} -N ${t1Nodes} -n ${t1Tasks} -t ${t1Time} --mem ${t1Mem} -o ${t1Log} ${trinP1})
+		job3ID=$(sed 's/Submitted batch job //g' <<< ${job3})
+			echo "Job 3: Trinity part 1; ${trinP1} queued with jobid=${job3ID}."
+			echo "${trinP1} will begin after successfull completion of ${derep}."
+			echo ""
+	# trinity phase 2 - dependent on trinity phase 1
+	job4=$(sbatch --dependency=afterok:${job3ID} -J ${t2JobName} -N ${t2Nodes} -n ${t2Tasks} -c ${t2CPUsPerTask} -t ${t2Time} --mem ${t2Mem} -o ${t2Log} -a ${t2Arrays} ${trinP2})
+		job4ID=$(sed 's/Submitted batch job //g' <<< ${job4})
+			echo "Job 4: Trinity part 2; ${trinP2} queued with jobid=${job4ID}."
+			echo "${trinP2} will begin after successfull completion of ${trinP1}."
+			echo ""
+	# trinity phase 3 - dependent on trinity phase 2
+	job5=$(sbatch --dependency=afterok:${job4ID} -J ${t3JobName} -N ${t3Nodes} -n ${t3Tasks} -c ${t3CPUsPerTask} -t ${t3Time} --mem ${t3Mem} -o ${t3Log} ${trinP3})
+		job5ID=$(sed 's/Submitted batch job //g' <<< ${job5})
+			echo "Job 5: Trinity part 3 (final); ${trinP3} queued with jobid=${job5ID}."
+			echo "${trinP3} will begin after successfull completion of ${trinP2}."
+			echo ""
+	# blast - dependent on trinity phase 3
+	job6=$(sbatch --dependency=afterok:${job5ID} -J ${blastJobName} -N ${blastNodes} -n ${blastTasks} -c ${blastCPUsPerTask} -t ${blastTime} --mem ${blastMem} -o ${blastLog} ${blastnTrin})
+		job6ID=$(sed 's/Submitted batch job //g' <<< ${job6})
+			echo "Job 6: blastN to nt; ${blastnTrin} queued with jobid=${job6ID}."
+			echo "${blastnTrin} will begin after successfull completion of ${trinP3}."
+			echo ""
+	# diamond - dependent on trinity phase 3
+	job7=$(sbatch --dependency=afterok:${job5ID} -J ${diamJobName} -N ${diamNodes} -n ${diamTasks} -c ${diamCPUsPerTask} -t ${diamTime} --mem ${diamMem} -o ${diamLog} ${diamTrin})
+		job7ID=$(sed 's/Submitted batch job //g' <<< ${job7})
+			echo "Job 7: Diamond blastX to nr; ${diamTrin} queued with jobid=${job7ID}."
+			echo "${diamTrin} will begin after successfull completion of ${trinP3}."
+			echo ""
 
-# derep
-job2=$(sbatch --dependency=afterok:${job1ID} -J ${drJobName} -N ${drNodes} -n ${drTasks} -c ${drCPUsPerTask} -t ${drTime} --mem ${drMem} -o ${drLog} ${derep})
+elif [[ "${step}" == "derep" ]]; then
+	# derep - no dependency
+	job2=$(sbatch -J ${drJobName} -N ${drNodes} -n ${drTasks} -c ${drCPUsPerTask} -t ${drTime} --mem ${drMem} -o ${drLog} ${derep})
+		job2ID=$(sed 's/Submitted batch job //g' <<< ${job2})
+			echo "Job 2: Dereplication; ${derep} queued with jobid=${job2ID}."
+			echo "${derep} will begin after successfull completion of ${qc}."
+			echo ""
+	# trinity phase 1 - dependent on derep
+	job3=$(sbatch --dependency=afterok:${job2ID} -J ${t1JobName} -N ${t1Nodes} -n ${t1Tasks} -t ${t1Time} --mem ${t1Mem} -o ${t1Log} ${trinP1})
+		job3ID=$(sed 's/Submitted batch job //g' <<< ${job3})
+			echo "Job 3: Trinity part 1; ${trinP1} queued with jobid=${job3ID}."
+			echo "${trinP1} will begin after successfull completion of ${derep}."
+			echo ""
+	# trinity phase 2  - dependent on trinity phase 1
+	job4=$(sbatch --dependency=afterok:${job3ID} -J ${t2JobName} -N ${t2Nodes} -n ${t2Tasks} -c ${t2CPUsPerTask} -t ${t2Time} --mem ${t2Mem} -o ${t2Log} -a ${t2Arrays} ${trinP2})
+		job4ID=$(sed 's/Submitted batch job //g' <<< ${job4})
+			echo "Job 4: Trinity part 2; ${trinP2} queued with jobid=${job4ID}."
+			echo "${trinP2} will begin after successfull completion of ${trinP1}."
+			echo ""
+	# trinity phase 3 - dependent on trinity phase 2
+	job5=$(sbatch --dependency=afterok:${job4ID} -J ${t3JobName} -N ${t3Nodes} -n ${t3Tasks} -c ${t3CPUsPerTask} -t ${t3Time} --mem ${t3Mem} -o ${t3Log} ${trinP3})
+		job5ID=$(sed 's/Submitted batch job //g' <<< ${job5})
+			echo "Job 5: Trinity part 3 (final); ${trinP3} queued with jobid=${job5ID}."
+			echo "${trinP3} will begin after successfull completion of ${trinP2}."
+			echo ""
+	# blast - dependent on trinity phase 3
+	job6=$(sbatch --dependency=afterok:${job5ID} -J ${blastJobName} -N ${blastNodes} -n ${blastTasks} -c ${blastCPUsPerTask} -t ${blastTime} --mem ${blastMem} -o ${blastLog} ${blastnTrin})
+		job6ID=$(sed 's/Submitted batch job //g' <<< ${job6})
+			echo "Job 6: blastN to nt; ${blastnTrin} queued with jobid=${job6ID}."
+			echo "${blastnTrin} will begin after successfull completion of ${trinP3}."
+			echo ""
+	# diamond  - dependent on trinity phase 3
+	job7=$(sbatch --dependency=afterok:${job5ID} -J ${diamJobName} -N ${diamNodes} -n ${diamTasks} -c ${diamCPUsPerTask} -t ${diamTime} --mem ${diamMem} -o ${diamLog} ${diamTrin})
+		job7ID=$(sed 's/Submitted batch job //g' <<< ${job7})
+			echo "Job 7: Diamond blastX to nr; ${diamTrin} queued with jobid=${job7ID}."
+			echo "${diamTrin} will begin after successfull completion of ${trinP3}."
+			echo ""
 
-	job2ID=$(sed 's/Submitted batch job //g' <<< ${job2})
-	
-	echo "Job 2: Dereplication; ${derep} queued with jobid=${job2ID}."
-	echo "${derep} will begin after successfull completion of ${qc}."
-	echo ""
+elif [[ "${step}" == "trinityP1" ]]; then
+	# trinity phase 1 - no dependency
+	job3=$(sbatch -J ${t1JobName} -N ${t1Nodes} -n ${t1Tasks} -t ${t1Time} --mem ${t1Mem} -o ${t1Log} ${trinP1})
+		job3ID=$(sed 's/Submitted batch job //g' <<< ${job3})
+			echo "Job 3: Trinity part 1; ${trinP1} queued with jobid=${job3ID}."
+			echo "${trinP1} will begin after successfull completion of ${derep}."
+			echo ""
+	# trinity phase 2 - dependent on trinity phase 1
+	job4=$(sbatch --dependency=afterok:${job3ID} -J ${t2JobName} -N ${t2Nodes} -n ${t2Tasks} -c ${t2CPUsPerTask} -t ${t2Time} --mem ${t2Mem} -o ${t2Log} -a ${t2Arrays} ${trinP2})
+		job4ID=$(sed 's/Submitted batch job //g' <<< ${job4})
+			echo "Job 4: Trinity part 2; ${trinP2} queued with jobid=${job4ID}."
+			echo "${trinP2} will begin after successfull completion of ${trinP1}."
+			echo ""
+	# trinity phase 3 - dependent on trinity phase 2
+	job5=$(sbatch --dependency=afterok:${job4ID} -J ${t3JobName} -N ${t3Nodes} -n ${t3Tasks} -c ${t3CPUsPerTask} -t ${t3Time} --mem ${t3Mem} -o ${t3Log} ${trinP3})
+		job5ID=$(sed 's/Submitted batch job //g' <<< ${job5})
+			echo "Job 5: Trinity part 3 (final); ${trinP3} queued with jobid=${job5ID}."
+			echo "${trinP3} will begin after successfull completion of ${trinP2}."
+			echo ""
+	# blast - dependent on trinity phase 3
+	job6=$(sbatch --dependency=afterok:${job5ID} -J ${blastJobName} -N ${blastNodes} -n ${blastTasks} -c ${blastCPUsPerTask} -t ${blastTime} --mem ${blastMem} -o ${blastLog} ${blastnTrin})
+		job6ID=$(sed 's/Submitted batch job //g' <<< ${job6})
+			echo "Job 6: blastN to nt; ${blastnTrin} queued with jobid=${job6ID}."
+			echo "${blastnTrin} will begin after successfull completion of ${trinP3}."
+			echo ""
+	# diamond - dependent on trinity phase 3
+	job7=$(sbatch --dependency=afterok:${job5ID} -J ${diamJobName} -N ${diamNodes} -n ${diamTasks} -c ${diamCPUsPerTask} -t ${diamTime} --mem ${diamMem} -o ${diamLog} ${diamTrin})
+		job7ID=$(sed 's/Submitted batch job //g' <<< ${job7})
+			echo "Job 7: Diamond blastX to nr; ${diamTrin} queued with jobid=${job7ID}."
+			echo "${diamTrin} will begin after successfull completion of ${trinP3}."
+			echo ""
 
-# trinity phase 1
-job3=$(sbatch --dependency=afterok:${job2ID} -J ${t1JobName} -N ${t1Nodes} -n ${t1Tasks} -t ${t1Time} --mem ${t1Mem} -o ${t1Log} ${trinP1})
+elif [[ "${step}" == "trinityP2" ]]; then
+	# trinity phase 2 - no dependency
+	job4=$(sbatch -J ${t2JobName} -N ${t2Nodes} -n ${t2Tasks} -c ${t2CPUsPerTask} -t ${t2Time} --mem ${t2Mem} -o ${t2Log} -a ${t2Arrays} ${trinP2})
+		job4ID=$(sed 's/Submitted batch job //g' <<< ${job4})
+			echo "Job 4: Trinity part 2; ${trinP2} queued with jobid=${job4ID}."
+			echo "${trinP2} will begin after successfull completion of ${trinP1}."
+			echo ""
+	# trinity phase 3- dependent on trinity phase 2
+	job5=$(sbatch --dependency=afterok:${job4ID} -J ${t3JobName} -N ${t3Nodes} -n ${t3Tasks} -c ${t3CPUsPerTask} -t ${t3Time} --mem ${t3Mem} -o ${t3Log} ${trinP3})
+		job5ID=$(sed 's/Submitted batch job //g' <<< ${job5})
+			echo "Job 5: Trinity part 3 (final); ${trinP3} queued with jobid=${job5ID}."
+			echo "${trinP3} will begin after successfull completion of ${trinP2}."
+			echo ""
+	# blast - dependent on trinity phase 3
+	job6=$(sbatch --dependency=afterok:${job5ID} -J ${blastJobName} -N ${blastNodes} -n ${blastTasks} -c ${blastCPUsPerTask} -t ${blastTime} --mem ${blastMem} -o ${blastLog} ${blastnTrin})
+		job6ID=$(sed 's/Submitted batch job //g' <<< ${job6})
+			echo "Job 6: blastN to nt; ${blastnTrin} queued with jobid=${job6ID}."
+			echo "${blastnTrin} will begin after successfull completion of ${trinP3}."
+			echo ""
+	# diamond - dependent on trinity phase 3
+	job7=$(sbatch --dependency=afterok:${job5ID} -J ${diamJobName} -N ${diamNodes} -n ${diamTasks} -c ${diamCPUsPerTask} -t ${diamTime} --mem ${diamMem} -o ${diamLog} ${diamTrin})
+		job7ID=$(sed 's/Submitted batch job //g' <<< ${job7})
+			echo "Job 7: Diamond blastX to nr; ${diamTrin} queued with jobid=${job7ID}."
+			echo "${diamTrin} will begin after successfull completion of ${trinP3}."
+			echo ""
 
-	job3ID=$(sed 's/Submitted batch job //g' <<< ${job3})
-	
-	echo "Job 3: Trinity part 1; ${trinP1} queued with jobid=${job3ID}."
-	echo "${trinP1} will begin after successfull completion of ${derep}."
-	echo ""
+elif [[ "${step}" == "trinityP3" ]]; then
+	# trinity phase 3 - no dependency
+	job5=$(sbatch -J ${t3JobName} -N ${t3Nodes} -n ${t3Tasks} -c ${t3CPUsPerTask} -t ${t3Time} --mem ${t3Mem} -o ${t3Log} ${trinP3})
+		job5ID=$(sed 's/Submitted batch job //g' <<< ${job5})
+			echo "Job 5: Trinity part 3 (final); ${trinP3} queued with jobid=${job5ID}."
+			echo "${trinP3} will begin after successfull completion of ${trinP2}."
+			echo ""
+	# blast - dependent on trinity phase 3
+	job6=$(sbatch --dependency=afterok:${job5ID} -J ${blastJobName} -N ${blastNodes} -n ${blastTasks} -c ${blastCPUsPerTask} -t ${blastTime} --mem ${blastMem} -o ${blastLog} ${blastnTrin})
+		job6ID=$(sed 's/Submitted batch job //g' <<< ${job6})
+			echo "Job 6: blastN to nt; ${blastnTrin} queued with jobid=${job6ID}."
+			echo "${blastnTrin} will begin after successfull completion of ${trinP3}."
+			echo ""
+	# diamond - dependent on trinity phase 3
+	job7=$(sbatch --dependency=afterok:${job5ID} -J ${diamJobName} -N ${diamNodes} -n ${diamTasks} -c ${diamCPUsPerTask} -t ${diamTime} --mem ${diamMem} -o ${diamLog} ${diamTrin})
+		job7ID=$(sed 's/Submitted batch job //g' <<< ${job7})
+			echo "Job 7: Diamond blastX to nr; ${diamTrin} queued with jobid=${job7ID}."
+			echo "${diamTrin} will begin after successfull completion of ${trinP3}."
+			echo ""
 
-# trinity phase 2
-job4=$(sbatch --dependency=afterok:${job3ID} -J ${t2JobName} -N ${t2Nodes} -n ${t2Tasks} -c ${t2CPUsPerTask} -t ${t2Time} --mem ${t2Mem} -o ${t2Log} -a ${t2Arrays} ${trinP2})
+elif [[ "${step}" == "blast+diamond" ]]; then
+	# blast - no dependency
+	job6=$(sbatch -J ${blastJobName} -N ${blastNodes} -n ${blastTasks} -c ${blastCPUsPerTask} -t ${blastTime} --mem ${blastMem} -o ${blastLog} ${blastnTrin})
+		job6ID=$(sed 's/Submitted batch job //g' <<< ${job6})
+			echo "Job 6: blastN to nt; ${blastnTrin} queued with jobid=${job6ID}."
+			echo "${blastnTrin} will begin after successfull completion of ${trinP3}."
+			echo ""
+	# diamond - no dependency
+	job7=$(sbatch -J ${diamJobName} -N ${diamNodes} -n ${diamTasks} -c ${diamCPUsPerTask} -t ${diamTime} --mem ${diamMem} -o ${diamLog} ${diamTrin})
+		job7ID=$(sed 's/Submitted batch job //g' <<< ${job7})
+			echo "Job 7: Diamond blastX to nr; ${diamTrin} queued with jobid=${job7ID}."
+			echo "${diamTrin} will begin after successfull completion of ${trinP3}."
+			echo ""
 
-	job4ID=$(sed 's/Submitted batch job //g' <<< ${job4})
+elif [[ "${step}" == "onlyQC" ]]; then
+	# QC - no dependency
+	job1=$(sbatch -J ${qcJobName} -N ${qcNodes} -n ${qcTasks} -c ${qcCPUsPerTask} -t ${qcTime} --mem ${qcMem} -o ${qcLog} ${qc})
+		job1ID=$(sed 's/Submitted batch job //g' <<< ${job1})
+			echo ""
+			echo "Job 1: Quality Trimming; ${qc} queued with jobid=${job1ID}."
+			echo ""
 
-	echo "Job 4: Trinity part 2; ${trinP2} queued with jobid=${job4ID}."
-	echo "${trinP2} will begin after successfull completion of ${trinP1}."
-	echo ""
-	
-# trinity phase 3
-job5=$(sbatch --dependency=afterok:${job4ID} -J ${t3JobName} -N ${t3Nodes} -n ${t3Tasks} -c ${t3CPUsPerTask} -t ${t3Time} --mem ${t3Mem} -o ${t3Log} ${trinP3})
+elif [[ "${step}" == "onlyDerep" ]]; then
+	# derep - no dependency
+	job2=$(sbatch -J ${drJobName} -N ${drNodes} -n ${drTasks} -c ${drCPUsPerTask} -t ${drTime} --mem ${drMem} -o ${drLog} ${derep})
+		job2ID=$(sed 's/Submitted batch job //g' <<< ${job2})
+			echo "Job 2: Dereplication; ${derep} queued with jobid=${job2ID}."
+			echo "${derep} will begin after successfull completion of ${qc}."
+			echo ""
 
-	job5ID=$(sed 's/Submitted batch job //g' <<< ${job5})
+elif [[ "${step}" == "onlyTrinity" ]]; then
+	# trinity phase 1 - no dependency
+	job3=$(sbatch -J ${t1JobName} -N ${t1Nodes} -n ${t1Tasks} -t ${t1Time} --mem ${t1Mem} -o ${t1Log} ${trinP1})
+		job3ID=$(sed 's/Submitted batch job //g' <<< ${job3})
+			echo "Job 3: Trinity part 1; ${trinP1} queued with jobid=${job3ID}."
+			echo "${trinP1} will begin after successfull completion of ${derep}."
+			echo ""
+	# trinity phase 2 - dependent on trinity phase 1
+	job4=$(sbatch --dependency=afterok:${job3ID} -J ${t2JobName} -N ${t2Nodes} -n ${t2Tasks} -c ${t2CPUsPerTask} -t ${t2Time} --mem ${t2Mem} -o ${t2Log} -a ${t2Arrays} ${trinP2})
+		job4ID=$(sed 's/Submitted batch job //g' <<< ${job4})
+			echo "Job 4: Trinity part 2; ${trinP2} queued with jobid=${job4ID}."
+			echo "${trinP2} will begin after successfull completion of ${trinP1}."
+			echo ""
+	# trinity phase 3 - dependent on trinity phase 2
+	job5=$(sbatch --dependency=afterok:${job4ID} -J ${t3JobName} -N ${t3Nodes} -n ${t3Tasks} -c ${t3CPUsPerTask} -t ${t3Time} --mem ${t3Mem} -o ${t3Log} ${trinP3})
+		job5ID=$(sed 's/Submitted batch job //g' <<< ${job5})
+			echo "Job 5: Trinity part 3 (final); ${trinP3} queued with jobid=${job5ID}."
+			echo "${trinP3} will begin after successfull completion of ${trinP2}."
+			echo ""
 
-	echo "Job 5: Trinity part 3 (final); ${trinP3} queued with jobid=${job5ID}."
-	echo "${trinP3} will begin after successfull completion of ${trinP2}."
-	echo ""
+elif [[ "${step}" == "onlyTrinityP1" ]]; then
+	# trinity phase 1 - no dependency
+	job3=$(sbatch -J ${t1JobName} -N ${t1Nodes} -n ${t1Tasks} -t ${t1Time} --mem ${t1Mem} -o ${t1Log} ${trinP1})
+		job3ID=$(sed 's/Submitted batch job //g' <<< ${job3})
+			echo "Job 3: Trinity part 1; ${trinP1} queued with jobid=${job3ID}."
+			echo "${trinP1} will begin after successfull completion of ${derep}."
+			echo ""
 
-# blast
-job6=$(sbatch --dependency=afterok:${job5ID} -J ${blastJobName} -N ${blastNodes} -n ${blastTasks} -c ${blastCPUsPerTask} -t ${blastTime} --mem ${blastMem} -o ${blastLog} ${blastnTrin})
-	
-	job6ID=$(sed 's/Submitted batch job //g' <<< ${job6})		
+elif [[ "${step}" == "onlyTrinityP2" ]]; then
+	# trinity phase 2 - no dependency
+	job4=$(sbatch -J ${t2JobName} -N ${t2Nodes} -n ${t2Tasks} -c ${t2CPUsPerTask} -t ${t2Time} --mem ${t2Mem} -o ${t2Log} -a ${t2Arrays} ${trinP2})
+		job4ID=$(sed 's/Submitted batch job //g' <<< ${job4})
+			echo "Job 4: Trinity part 2; ${trinP2} queued with jobid=${job4ID}."
+			echo "${trinP2} will begin after successfull completion of ${trinP1}."
+			echo ""
 
-	echo "Job 6: blastN to nt; ${blastnTrin} queued with jobid=${job6ID}."
-	echo "${blastnTrin} will begin after successfull completion of ${trinP3}."
-	echo ""
+elif [[ "${step}" == "onlyTrinityP3" ]]; then
+	# trinity phase 3 - no dependency
+	job5=$(sbatch -J ${t3JobName} -N ${t3Nodes} -n ${t3Tasks} -c ${t3CPUsPerTask} -t ${t3Time} --mem ${t3Mem} -o ${t3Log} ${trinP3})
+		job5ID=$(sed 's/Submitted batch job //g' <<< ${job5})
+			echo "Job 5: Trinity part 3 (final); ${trinP3} queued with jobid=${job5ID}."
+			echo "${trinP3} will begin after successfull completion of ${trinP2}."
+			echo ""
 
-# diamond
-job7=$(sbatch --dependency=afterok:${job5ID} -J ${diamJobName} -N ${diamNodes} -n ${diamTasks} -c ${diamCPUsPerTask} -t ${diamTime} --mem ${diamMem} -o ${diamLog} ${diamTrin})
+elif [[ "${step}" == "onlyBlast" ]]; then
+	# blast - no dependency
+	job6=$(sbatch -J ${blastJobName} -N ${blastNodes} -n ${blastTasks} -c ${blastCPUsPerTask} -t ${blastTime} --mem ${blastMem} -o ${blastLog} ${blastnTrin})
+		job6ID=$(sed 's/Submitted batch job //g' <<< ${job6})
+			echo "Job 6: blastN to nt; ${blastnTrin} queued with jobid=${job6ID}."
+			echo "${blastnTrin} will begin after successfull completion of ${trinP3}."
+			echo ""
 
-	job7ID=$(sed 's/Submitted batch job //g' <<< ${job7})		
-	
-	echo "Job 7: Diamond blastX to nr; ${diamTrin} queued with jobid=${job7ID}."
-	echo "${diamTrin} will begin after successfull completion of ${trinP3}."
-	echo ""
+elif [[ "${step}" == "onlyDiamond" ]]; then
+	# diamond - no dependency
+	job7=$(sbatch -J ${diamJobName} -N ${diamNodes} -n ${diamTasks} -c ${diamCPUsPerTask} -t ${diamTime} --mem ${diamMem} -o ${diamLog} ${diamTrin})
+		job7ID=$(sed 's/Submitted batch job //g' <<< ${job7})
+			echo "Job 7: Diamond blastX to nr; ${diamTrin} queued with jobid=${job7ID}."
+			echo "${diamTrin} will begin after successfull completion of ${trinP3}."
+			echo ""
 
+else
+	echo "-s <step> not an accepted value. Options are all, QC, derep, trinityP1, trinityP2, trinityP3, blast+diamond, onlyBlast, onlyDiamond"
+	date
+	exit 1
+fi
+date
--- init.sh
+++ init.sh
@@ -11,12 +11,10 @@ helpMessage() {
 	exit 1;
 }
 
-flowControl() {
+flowControl() {							# $1 is error message
 	if [ $? -ne 0 ]
 	then
 		echo $1; date; exit 1;
-	else
-		echo $2;
 	fi
 }
 
@@ -28,29 +26,25 @@ do
 	        h) helpMessage;;
 	        f) R1=${OPTARG};;
 	        r) R2=${OPTARG};;
-			o) outputDir=${OPTARG};;
+					o) outputDir=${OPTARG};;
 	esac
 done
 shift $((OPTIND - 1))
 
-# check input files
-if [ ! -f "${R1}" ]
-then
+# Check R1 exists
+if [ ! -f "${R1}" ]; then
 	echo "Input R1 file does not exist."; date; exit 1
 fi
-##
-if [ ! -f "${R2}" ]
-then
+# Check R2 exists
+if [ ! -f "${R2}" ]; then
 	echo "Input R2 file does not exits."; date; exit 1
 fi
-##
-if [ "${R1}" == "${R2}" ]
-then
+# Check R1 & R2 are not the same file
+if [ "${R1}" == "${R2}" ]; then
 	echo "Input error: -f and -r cannot be the same."; date; exit 1
 fi
-#
-if [ -d "${outputDir}" ]
-then
+# Check if outputDir already exists - cancel overwrite
+if [ -d "${outputDir}" ]; then
 	echo "Output directory already exists. Cannot overwrite."; date; exit 1
 fi
 
@@ -60,44 +54,36 @@ echo "SampleID = ${sampleID}"
 
 # setting up direcory structure
 outDir=${outputDir}/${sampleID} 	# All output goes here
-scriptsDir=${outDir}/scripts 		# Scripts are stored and launched from here
+scriptsDir=${outDir}/scripts 			# Scripts are stored and launched from here
 mkdir -p ${outDir}
 mkdir ${scriptsDir}
-mkdir ${outDir}/logs 				# All slurm log files will do here
+mkdir ${outDir}/logs 							# All slurm log files will do here
 
 # copy raw data to sample working directory
-cp ${R1} ${outDir}/
-flowControl "Copying raw R1 failed." "${R1} copied to ${outDir}."
-cp ${R2} ${outDir}/
-flowControl "Copying raw R2 failed." "${R2} copied to ${outDir}." 
+cp ${R1} ${outDir}/ && flowControl "Copying ${R1} to ${outDir} failed."
+cp ${R2} ${outDir}/ && flowControl "Copying ${R2} to ${outDir} failed."
 
-# copy analysis scripts to sample scripts directory & editing scripts with universal variables
+# copy analysis scripts to working directory & editing scripts with universal variables
 for x in /datasets/work/hb-austicks/work/Project_Phoenix/data/MetaTransTick_hpc_pipeline/bin/*.slurm.sh
 do
 	newFilename=$(basename $x .slurm.sh).${sampleID}.slurm.sh
-	cp ${x} ${scriptsDir}/${newFilename}
-	flowControl "Copying scripts failed." "Copying scripts..."
-	sed -i s@SAMPLEID@${sampleID}@g ${scriptsDir}/${newFilename} 	# SAMPLEID is the sample ID placeholder in the master scripts.
-	flowControl "Writing SAMPLEID variable failed."
-	sed -i s@OUTDIR@${outDir}@g ${scriptsDir}/${newFilename} 	# OUTDIR is the output directory name placeholder in the master scripts.
-	flowControl "Writing OUTDIR variable failed."
+	cp ${x} ${scriptsDir}/${newFilename} && flowControl "Copying scripts failed." "Copying scripts..."
+	sed -i s@SAMPLEID@${sampleID}@g ${scriptsDir}/${newFilename} && flowControl "Writing SAMPLEID variable failed."
+	sed -i s@OUTDIR@${outDir}@g ${scriptsDir}/${newFilename} && flowControl "Writing OUTDIR variable failed."
 done
 
-# copy run script
+# copy run.sh to working dir and add universal variables
 runScript=/datasets/work/hb-austicks/work/Project_Phoenix/data/MetaTransTick_hpc_pipeline/bin/run.sh
 newRunScript=$(basename $runScript .sh).${sampleID}.sh
-cp ${runScript} ${scriptsDir}/${newRunScript}
-flowControl "Copying run.sh failed." "Copying run.sh complete."
-sed -i s/SAMPLEID/${sampleID}/g ${scriptsDir}/${newRunScript} 	# SAMPLEID is the sample placeholder in the master scripts
-flowControl "Writing SAMPLEID variable to run.sh failed."
+cp ${runScript} ${scriptsDir}/${newRunScript} && flowControl "Copying run.sh failed." "Copying run.sh complete."
+sed -i s/SAMPLEID/${sampleID}/g ${scriptsDir}/${newRunScript} && flowControl "Writing SAMPLEID variable to run.sh failed."
 
-# copy slurmParams.txt
+# copy slurmParams.txt to working dir and add universal variables
 slurmParams=/datasets/work/hb-austicks/work/Project_Phoenix/data/MetaTransTick_hpc_pipeline/bin/slurmParams.txt
-cp ${slurmParams} ${scriptsDir}/slurmParams.txt
-flowControl "Copying slurmParams.txt failed." "Copying slurmParams.txt complete."
-sed -i s@SAMPLEID@${sampleID}@g ${scriptsDir}/slurmParams.txt
-flowControl "Writing SAMPLEID variable to slurmParams.txt failed."
+cp ${slurmParams} ${scriptsDir}/slurmParams.txt && flowControl "Copying slurmParams.txt failed." "Copying slurmParams.txt complete."
+sed -i s@SAMPLEID@${sampleID}@g ${scriptsDir}/slurmParams.txt && flowControl "Writing SAMPLEID variable to slurmParams.txt failed."
 
+# final comments to stdout
 echo "Output and tmp directories and analysis scripts created..."
 echo ""
 echo "Run commands:"
@@ -105,4 +91,3 @@ echo "cd ${outDir}/scripts"
 echo "nohup run.${sampleID}.sh &"
 echo ""
 date
-
